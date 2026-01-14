ec66cc83f69e6c77f4687aff8997ca732be833a5
gabkings <gabworks51@gmail.com>
2026-01-14 16:09:33 +0300

    Updating cart price and quantity

--- cart/cart.py
+++ cart/cart.py
@@ -81,3 +81,15 @@ class Cart():
             del self.cart[product_id]
 
         self.session.modified = True
+
+
+    def update(self, product, qty):
+
+        product_id = str(product)
+        product_quantity = qty
+
+        if product_id in self.cart:
+
+            self.cart[product_id]['qty'] = product_quantity
+
+        self.session.modified = True
